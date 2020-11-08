df5b92c5a0a8b055ab3f4745b493ca0e954ad61c
yusufso <y.hussain123@outlook.com>
2020-11-08 19:24:38 +0000

    spelling error

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let apiKey = 'f888cee9980bb06bd1966d5927d6376c91b84c7576ee536bdc91ce2ffef99bc7';
  
 import GetCandleData from './GetCandleData.js';
-import DrawCandleChart from './DrawCandleData.js';
+import DrawCandleChart from './DrawCandleChart.js';
 
 //GetCandleData(booBtcToEth, howLong, apiKey, callback function)
 GetCandleData(true, 5, apiKey, (data) => {
